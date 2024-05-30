04e3e915fdb221bea5130c64fa9738904bfec7aa
pankajkhuswaha <khuswahapankaj00@gmail.com>
2024-05-30 18:24:00 +0530

    add : db connect

--- src/app/api/(routes)/auth/route.ts
+++ src/app/api/(routes)/auth/route.ts
@@ -1,6 +1,8 @@
+import dbconnect from "@/configs/dbconnect";
 import User from "@/models/user.model";
 import { NextResponse } from "next/server";
 
+dbconnect();
 export async function GET() {
   const users = await User.find();
   return NextResponse.json(users);

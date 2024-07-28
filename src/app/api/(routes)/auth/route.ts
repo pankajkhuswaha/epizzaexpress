40ddf0a766ad16c26a5b0da886750e2df828eb77
pankajkhuswaha <khuswahapankaj00@gmail.com>
2024-07-28 14:04:14 +0530

    fix : route

--- src/app/api/(routes)/auth/route.ts
+++ src/app/api/(routes)/auth/route.ts
@@ -2,8 +2,8 @@ import dbconnect from "@/configs/dbconnect";
 import User from "@/models/user.model";
 import { NextResponse } from "next/server";
 
-dbconnect();
 export async function GET() {
+  await dbconnect();
   const users = await User.find();
   return NextResponse.json(users);
 }
